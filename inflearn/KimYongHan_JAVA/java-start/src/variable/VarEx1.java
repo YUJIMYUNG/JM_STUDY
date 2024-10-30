3d1ff99e07baf7084a2118d6711df7cce8f6e5d6
JIMYUNG_YU <jm_7516@naver.com>
2024-10-30 16:11:22 +0900

    1029 김영한의 자바 입문 - 변수

---
+++ inflearn/KimYongHan_JAVA/java-start/src/variable/VarEx1.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        // 다음 코드에 반복해서 나오는 숫자 4, 3을 다른 숫자로 한 번에 변경할 수 있ㄷ로ㅗㄱ
+        // 다음을 변수 num1, num2 를 사용하도록 변경해보세요.
+        System.out.println(4 + 3);
+        System.out.println(4 - 3);
+        System.out.println(4 * 3);
+
+
+        // 풀이
+        int num1 = 4;
+        int num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+}
---
+++ inflearn/KimYongHan_JAVA/java-start/src/variable/VarEx3.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        // 문제
+        // long 타입의 변수를 선언하고, 그 변수를 10000000000(백억)으로 초기화한 후 출력하는 프로그램을 작성하세요.
+        // boolean 타입의 변수를 선언하고, 그 변수를 true로 초기화 한 후 출력하는 프로그램을 작성하세요.
+
+        // 풀이
+        long num1 = 10000000000L;
+        System.out.println(num1);
+
+        boolean var1 = true;
+        System.out.println(var1);
+    }
+}
