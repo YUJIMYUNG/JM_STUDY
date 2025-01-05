3964626cf6eafd0f0e767e8fbe0870844f06ccc2
JIMYUNG_YU <jm_7516@naver.com>
2025-01-05 23:35:36 +0900

    1005 웹개발과제 - Java(spring) todoService - js -  fetch처리 완료

--- homework/kor2024_webDev_spring/src/main/resources/static/day55task/todo.js
+++ homework/kor2024_webDev_spring/src/main/resources/static/day55task/todo.js
@@ -8,9 +8,123 @@ let todoList = [
 let todoNoPK = 3;
 
 // 3. 등록함수
+function register(){    
+    console.log("등록함수 실행");
+
+    //1. HTML로부터 input DOM객체 가져오기
+    const conInput = document.querySelector('.conInput');
+    console.log(conInput);
+
+    //2. 가져온 객체의 value속성 가져오기
+    const getTodoContent = conInput.value;
+    console.log(getTodoContent);
+
+    //3. 할일 객체 구성
+    const todo =  {
+        todoNo : todoNoPK, todoContent : getTodoContent, todoState : false,
+    }
+
+    //4. 구성한 객체를 배열에 저장
+    todoList.push(todo);
+
+    // fetch 처리
+    let option = {
+        method : 'POST',
+        headers : {'Content-Type' : 'application/json'},
+        body : JSON.stringify(todo)
+    }
+
+    fetch('/register', option)
+        .then(respone => respone.json())
+        .then(data => {console.log(data);})
+        .catch(error => {console.log(error);})
+
+    //객체 저장 완료시 할일코드 +1
+    todoNoPK++;
+
+    //결과 및 종료
+    alert("할일 등록 되었습니다.");
+    conInput.value = ``;
+    view();
+    return;
+}
 
 // 4. 출력함수
+view();
+function view(){
+    console.log('출력함수 실행');
+
+    const todoBottom = document.querySelector('.todoBottom');
+
+    let HTML = '';
+    
+    for(let i = 0; i < todoList.length ; i++){
+        const todo = todoList[i];
+        HTML += `<div class = "conBox ${todo.todoState ? 'success' : ''}">
+                <div class = "cont"> ${todo.todoContent} </div>
+                <div class="conBtns">
+                    <button onclick = "modify(${todo.todoNo})" class="updateBtn"> 수정 </button>
+                    <button onclikc = "deleteTodo(${todo.todoNo})" class = "deleteBtn"> 삭제 </button>
+                </div>
+            </div>`
+    }
+    
+    // fetch 처리
+    fetch('/view')
+    .then(r => r.json())
+    .then(data => {console.log(data)})
+    .catch(error => {console.log(error)})
+    
+    todoBottom.innerHTML = HTML;
+}
 
 // 5. 수정함수
+function modify(todoNo){
+    console.log(`${todoNo} 수정함수`);
+
+    let modifyTodoState = '';
+
+    for(let i = 0; i < todoList.length ; i++ ){
+        if(todoList[i].todoNo == todoNo){
+            let todoState = todoList[i].todoState;
+            modifyTodoState = todoList[i].todoState = !todoState;
+            break;
+        }
+    }
+
+    //fetch 처리
+    let option2 = {
+        method : 'PUT',
+        headers : {'Content-Type' : 'application/json'},
+        body : JSON.stringify({'todoNo' : todoNo, 'todoState' : modifyTodoState})
+    }
+
+    fetch('/modify', option2)
+        .then(r => r.json())
+        .then(data => {console.log(data)})
+        .catch(error => {console.log(error)})
+
+    view();
+    return;
+}
 
 // 6. 삭제함수
+function deleteTodo(todoNo) {
+    console.log(`${todoNo} 삭제함수`);
+    
+    for(let i = 0; i < todoList.length ; i++){
+        if(todoList[i].todoNo == todoNo){
+            todoList.splice(i, 1);
+            break;
+        }
+    }
+
+    // fetch 처리
+    fetch(`/delete?todoNo=${todoNo}`, {method : 'delete'})
+        .then(r => r.json())
+        .then(data => {console.log(data)})
+        .catch(error =>{console.log(error)})
+
+    view();
+    return;
+}
