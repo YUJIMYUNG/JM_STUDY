7f3163478a695ea49c2516130eac395272995ba1
JIMYUNG_YU <jm_7516@naver.com>
2025-03-27 17:40:57 +0900

    레포지토리 옮김

--- ReactSpringPrjoject/src/main/java/web/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package web.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import web.model.entity.CategoryEntity;
-
-public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
-}
--- ReactSpringPrjoject/src/main/java/web/repository/RecordRepository.java
+++
@@ -1,7 +0,0 @@
-package web.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import web.model.entity.RecordEntity;
-
-public interface RecordRepository extends JpaRepository<RecordEntity, Integer> {
-}
